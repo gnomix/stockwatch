6f59ef0d573559b65300dafbec3d3f94ae3500f7
mo <email@solidware.ca>
2011-03-17 13:21:22 -0600

    add application icon.

--- product/desktop.ui/Properties/AssemblyInfo.cs
+++ product/desktop.ui/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2011.3.17.1312")]
-[assembly: AssemblyFileVersion("2011.3.17.1312")]
+[assembly: AssemblyVersion("2011.3.17.1316")]
+[assembly: AssemblyFileVersion("2011.3.17.1316")]

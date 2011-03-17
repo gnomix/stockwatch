145f304cae06bf18d3cbac447e4ce94b752bff16
mo <email@solidware.ca>
2011-03-16 21:31:50 -0600

    move tax calculation out to view model.

--- product/desktop.ui/presenters/TaxSummaryPresenter.cs
+++ product/desktop.ui/presenters/TaxSummaryPresenter.cs
@@ -29,36 +29,17 @@ namespace desktop.ui.presenters
 
         public void notify(AddIncomeCommandMessage message)
         {
-            Selected.TotalIncome += message.amount;
-            if (Selected.TotalIncome <= 41544.00m)
-            {
-                Selected.Taxes = ((Selected.TotalIncome - 0m)*0.15m) + 0m;
-            }
-            if (Selected.TotalIncome > 41544.00m && Selected.TotalIncome <= 83088.00m)
-            {
-                Selected.Taxes = ((Selected.TotalIncome - 41544m)*0.22m) + 6232m;
-            }
-            if (Selected.TotalIncome > 83088.00m && Selected.TotalIncome <= 128800.00m)
-            {
-                Selected.Taxes = ((Selected.TotalIncome - 83088m)*0.26m) + 15371m;
-            }
-            if (Selected.TotalIncome > 128800.00m)
-            {
-                Selected.Taxes = ((Selected.TotalIncome - 128800m)*0.29m) + 27256m;
-            }
-            Selected.update(x => x.Taxes, x => x.TotalIncome);
+            Selected.AddIncome(message.amount);
         }
 
         public void notify(SelectedFamilyMember message)
         {
-            if (!people.ContainsKey(message.id))
-            {
-                people[message.id] = new TaxesForIndividual();
-            }
-            Selected = people[message.id];
+            if (!family.ContainsKey(message.id))
+                family[message.id] = new TaxesForIndividual();
+            Selected = family[message.id];
             update(x => x.Selected);
         }
 
-        IDictionary<Guid, TaxesForIndividual> people = new Dictionary<Guid, TaxesForIndividual>();
+        IDictionary<Guid, TaxesForIndividual> family = new Dictionary<Guid, TaxesForIndividual>();
     }
 }

1d6aa4f663b6cbcf9df1bf20b09f1bac8450963c
mo khan <mo@mokhan.ca>
2011-03-18 16:22:23 -0600

    toying with db4o database.

---
+++ product/service/orm/DB4ODatabase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Db4objects.Db4o;
+using solidware.financials.service.domain;
+
+namespace solidware.financials.service.orm
+{
+    public class DB4ODatabase : PersonRepository
+    {
+        IObjectContainer session;
+
+        public DB4ODatabase(IObjectContainer session)
+        {
+            this.session = session;
+            using (var embeddedObjectContainer = Db4oEmbedded.OpenFile("c:/tmp")) { }
+        }
+
+        public void save(Person person)
+        {
+            session.Store(person);
+        }
+
+        public Person find_by(Guid id)
+        {
+            return session.Query<Person>().Single(x => x.id == id);
+        }
+
+        public IEnumerable<Person> find_all()
+        {
+            return session.Query<Person>();
+        }
+    }
+}

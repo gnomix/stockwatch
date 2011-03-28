78033b57c3f34e6bc0a411442fba7eb508d590df
mo khan <mo@mokhan.ca>
2011-03-28 13:01:14 -0600

    start clean up for xaml for tax summary tab.

--- product/desktop.ui/model/TaxesForIndividual.cs
+++ product/desktop.ui/model/TaxesForIndividual.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace solidware.financials.windows.ui.model
 {
@@ -7,11 +8,25 @@ namespace solidware.financials.windows.ui.model
         public TaxesForIndividual(Guid id)
         {
             Id = id;
+            ProvincialTaxesGrid = new List<TaxRow>
+                                  {
+                                      new TaxRow {Name = "john doe", Tax = 23456.09m},
+                                      new TaxRow {Name = "sally doe", Tax = 9456.09m},
+                                  };
+            FederalTaxesGrid = new List<TaxRow>
+                               {
+                                   new TaxRow {Name = "john doe", Tax = 23456.09m},
+                                   new TaxRow {Name = "sally doe", Tax = 9456.09m},
+                               };
         }
 
         public Guid Id { get; private set; }
         public decimal TotalIncome { get; private set; }
+        public decimal TotalFamilyIncome { get; private set; }
         public decimal FederalTaxes { get; private set; }
+        public decimal FederalFamilyTaxes { get; private set; }
+        public IEnumerable<TaxRow> ProvincialTaxesGrid { get; private set; }
+        public IEnumerable<TaxRow> FederalTaxesGrid { get; private set; }
 
         public void AddIncome(decimal amount)
         {
@@ -19,6 +34,11 @@ namespace solidware.financials.windows.ui.model
             FederalTaxes = new FederalTaxes().CalculateFederalTaxesFor(TotalIncome);
             update(x => x.FederalTaxes, x => x.TotalIncome);
         }
+    }
 
+    public class TaxRow
+    {
+        public string Name { get; set; }
+        public decimal Tax { get; set; }
     }
 }

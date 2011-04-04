7f679b9f2ed6723213fb03a6c41d95e6b0e59813
mo <email@solidware.ca>
2011-04-03 20:54:58 -0600

    add UserControl for viewing a single stock.

---
+++ product/desktop.ui/views/SingleStockTab.xaml.cs
@@ -0,0 +1,12 @@
+﻿using solidware.financials.windows.ui.presenters;
+
+namespace solidware.financials.windows.ui.views
+{
+    public partial class SingleStockTab : View<SingleStockPresenter>
+    {
+        public SingleStockTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
